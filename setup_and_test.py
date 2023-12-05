1e617d4ecdb5600aa84e206a5e169731eb57afc8
Øyvind Lundesgaard <oyvind.lundesgaard@npolar.no>
2023-12-06 00:28:45 +0100

    Updated the setup file (now using a python script). Added testing notebook. Made the default map size a bit smaller.

--- setup_and_test.py
+++ setup_and_test.py
@@ -1,24 +1,17 @@
 import subprocess
 
-# List of commands to execute
-commands = [
-    # Create Conda environment
-    "mamba env create -f setup/ctd_env.yml",
-    
-    # Activate Conda environment
-    "mamba activate ctdenv",
-    
-    # Install Jupyter kernel
-    "python -m ipykernel install --user --name=ctdenv",
-    
-    # Open Jupyter Lab
-    "jupyter lab",
-]
+# Create Conda environment
+print('## CTD WORKSHOP: Installing a mamba environment called "ctdenv" (this may take a minute or two)..\n')
+subprocess.run("sleep 4", shell=True, check=True)
+subprocess.run("mamba env create -f setup/ctd_env.yml", shell=True, check=True)
 
-# Function to run commands
-def run_commands():
-    for command in commands:
-        subprocess.run(command, shell=True, check=True)
+# Install Jupyter kernel
+print('## CTD WORKSHOP: Installing jupyter kernel for running Jupyter Lab in the "ctdenv" environment.\n')
+print('(disregard the debugger warning - it does not matter for our purposes).')
+subprocess.run("sleep 4", shell=True, check=True)
+subprocess.run("conda run -n ctdenv python -m ipykernel install --user --name=ctdenv", shell=True, check=True)
 
-if __name__ == "__main__":
-    run_commands()
+# Open Jupyter Lab
+print('## CTD WORKSHOP: Success - hooray! Opening Jupyter Lab (a browser window should now pop up!)')
+print('(To cancel the Jupyter instance, press CTRL+C)')
+subprocess.run("conda run jupyter lab setup/Test_your_setup.ipynb", shell=True, check=True)
