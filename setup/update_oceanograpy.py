7fa5172beb30c0bd82af0be03156d61271bb5d7d
Øyvind Lundesgaard <oyvind.lundesgaard@npolar.no>
2023-12-07 11:35:38 +0100

    Update update_oceanograpy.py

--- setup/update_oceanograpy.py
+++ setup/update_oceanograpy.py
@@ -4,6 +4,6 @@ import time
 # Create Conda environment
 print('## CTD WORKSHOP: Updating the oceanograpy version in "ctdenv"..\n')
 time.sleep(4)
-subprocess.run('conda run -n ctdenv pip uninstall oceanograpy', shell=True, check=True)
+subprocess.run('conda run -n ctdenv pip uninstall -y oceanograpy', shell=True, check=True)
 subprocess.run("conda run -n ctdenv pip install --upgrade git+https://github.com/NPIOcean/oceanograpy.git", shell=True, check=True)
 
